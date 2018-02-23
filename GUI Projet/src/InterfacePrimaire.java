50b8dd9170fc3bb06d235724744cd26b22b034f1
csp1989a <csp1989a@r-info-diam-e06.iut.iut-tlse3.fr>
2018-02-23 14:17:47 +0100

    test

---
+++ GUI Projet/src/InterfacePrimaire.java
@@ -0,0 +1,20 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class InterfacePrimaire extends JPanel{
+	
+	public InterfacePrimaire() {
+		;
+		this.setLayout(new BorderLayout());
+		
+		JPanel panneauLateralGauche = new JPanel();
+		
+		this.add(panneauLateralGauche,BorderLayout.NORTH);
+		JButton Bcourses = new JButton();
+	}
+	
+	
+
+}
